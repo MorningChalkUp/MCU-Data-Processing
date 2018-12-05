f79ae53abbe1ab72f67ccc5f6abb3c61955045f4
Eric Sherred <eric@ericsherred.com>
2018-12-05 10:40:52 -0500

    update button

--- resources/views/emails/paymentReminder.blade.php
+++ resources/views/emails/paymentReminder.blade.php
@@ -11,7 +11,18 @@
 
       <p>Your first ad buy will be starting on {{ $data['ad_date'] }}. Don't forget to complete your payment beforehand by visiting <a href="https://ads.morningchalkup.com/p?{{ $data['order_id'] }}">here</a>.</p>
 
-      <p><a href="https://ads.morningchalkup.com/p?{{ $data['order_id'] }}" class="button-a">Pay Your Balance</a></p>
+      <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: auto;">
+        <tbody>
+          <tr>
+            <td style="border-radius: 3px; background: #3d5ba9; text-align: center;" class="button-td">
+              <a href="https://ads.morningchalkup.com/p?{{ $data['order_id'] }}" style="background: #3d5ba9; border: 15px solid #3d5ba9; font-family: Roboto, sans-serif; font-size: 16px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                <span style="color:#ffffff;" class="button-link">&nbsp;&nbsp;&nbsp;&nbsp; Pay Your Balance &nbsp;&nbsp;&nbsp;&nbsp;</span>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
     </td>
   </tr>
   <tr>
