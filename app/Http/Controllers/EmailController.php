5c48c062c58c89b0f51cb9dca75a1b2bf696bfc6
Eric Sherred <eric@ericsherred.com>
2018-12-07 11:40:25 -0500

    or rather actual date handling

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -42,8 +42,8 @@ class EmailController extends Controller
       'name' => $request->user['name'],
       'email' => $request->user['email'],
       'week_id' => $request->week_id,
-      'ad_date' => new Carbon($request->ad_date)->format('F Y jS'),
-      'ad_deadline' => new Carbon($request->ad_date)->sub('5 days')->format('l F Y j'),
+      'ad_date' => (new Carbon($request->ad_date))->format('F Y jS'),
+      'ad_deadline' => (new Carbon($request->ad_date))->sub('5 days')->format('l F Y j'),
       'date' => Carbon::today()->toFormattedDateString(),
     );
 
